83fae4e91376d2d35b729245d19fe699fa0288a4
scottrippey <kokanut82@hotmail.com>
2011-01-03 18:16:11 -0800

    Added comments and TextWriter extension, and simplified "FormatAndJoin" method.

--- src/SmartFormat/SmartExtensions.cs
+++ src/SmartFormat/SmartExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,52 +12,65 @@ namespace SmartFormat
     {
         #region: StringBuilder :
 
+        /// <summary> Appends a formatted string, using the same semantics as Smart.Format. </summary>
+        /// <param name="writer">The StringBuilder that will be used for output</param>
+        /// <param name="format">The template that defines how the arguments are formatted</param>
+        /// <param name="args">A list of arguments to be used in formatting</param>
         public static void AppendSmart(this StringBuilder sb, string format, params object[] args)
         {
-            var so = new StringOutput(sb);
-            Smart.Default.FormatInto(so, format, args);
+            var output = new StringOutput(sb);
+            Smart.Default.FormatInto(output, format, args);
+        }
+
+        #endregion
+
+        #region: TextWriter :
+
+        /// <summary> Writes out a formatted string, using the same semantics as Smart.Format. </summary>
+        /// <param name="writer">The TextWriter that will be used for output</param>
+        /// <param name="format">The template that defines how the arguments are formatted</param>
+        /// <param name="args">A list of arguments to be used in formatting</param>
+        public static void WriteSmart(this System.IO.TextWriter writer, string format, params object[] args)
+        {
+            var output = new TextWriterOutput(writer);
+            Smart.Default.FormatInto(output, format, args);
         }
 
         #endregion
 
         #region: String :
 
-        // I can't really decide on the correct naming convention for these extensions,
-        // so for now I'm just including Format, SmartFormat, and FormatSmart:
-
-        public static string Format(this string format, params object[] args)
-        {
-            return Smart.Format(format, args);
-        }
-
-        public static string SmartFormat(this string format, params object[] args)
-        {
-            return Smart.Format(format, args);
-        }
-
-        public static string SmartFormat(this string format, ref FormatCache cache, params object[] args)
-        {
-            return Smart.Default.FormatWithCache(ref cache, format, args);
-        }
-
+        /// <summary> Formats the specified arguments using this string as a template. </summary>
+        /// <param name="format">The template that defines how the arguments are formatted</param>
+        /// <param name="args">A list of arguments to be used in formatting</param>
         public static string FormatSmart(this string format, params object[] args)
         {
             return Smart.Format(format, args);
         }
 
+        /// <summary> Formats the specified arguments using this string as a template. 
+        /// Caches the parsing results for increased performance.
+        /// </summary>
+        /// <param name="format">The template that defines how the arguments are formatted</param>
+        /// <param name="args">A list of arguments to be used in formatting</param>
+        /// <param name="cache">Outputs an object that increases performance if the same format string is used repeatedly.</param>
         public static string FormatSmart(this string format, ref FormatCache cache, params object[] args)
         {
             // With cache:
             return Smart.Default.FormatWithCache(ref cache, format, args);
         }
 
-
-
         #endregion
 
         #region: Enumerable :
 
-        public static string FormatAndJoin<T>(this IEnumerable<T> sources, string separator, string format, Func<T, object[]> args)
+        /// <summary> Formats each source item using the specified format string.
+        /// Each item is separated by the separator.
+        /// </summary>
+        /// <param name="sources">A collection of objects to be used in formatting</param>
+        /// <param name="separator">The text to insert between items</param>
+        /// <param name="format">The template that defines how each source is formatted</param>
+        public static string FormatAndJoin<T>(this IEnumerable<T> sources, string format, string separator)
         {
             // We're gonna need to count the items anyway, so let's not enumerate twice:
             var sourceList = sources as IList<T> ?? sources.ToList();
@@ -67,7 +80,7 @@ namespace SmartFormat
             for (int i = 0; i < sourceList.Count; i++)
             {
                 if (i > 0) output.Write(separator);
-                formatter.FormatWithCacheInto(ref cache, output, format, args(sourceList[i]));
+                formatter.FormatWithCacheInto(ref cache, output, format, sourceList[i]);
             }
             return output.ToString();
         }
