78437f9a14c02d2cc1eb8c8bf8d08c2092d19b4a
Jesse Plamondon-Willard <plamondon@brainbankinc.com>
2013-05-13 11:55:11 -0400

    + GlobalAssemblyInfo so we can change assembly version in one place.

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SmartFormat.NET")]
+[assembly: AssemblyCopyright("Copyright 2012 Scott Rippey")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
