bff8deeed56021752815fdc897825e0be0ae426a
Bernhard Millauer <bernhard.millauer@update.com>
2013-08-02 16:17:14 +0200

    Added a test for dynamic values;

--- src/SmartFormat.Tests/Extensions/DictionaryFormatterTests.cs
+++ src/SmartFormat.Tests/Extensions/DictionaryFormatterTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
@@ -35,6 +36,18 @@ namespace SmartFormat.Tests
             };
         }
 
+		public dynamic GetDynamicArgs()
+		{
+			dynamic d = new ExpandoObject();
+			d.Numbers = new Dictionary<string, object> { { "One", 1 }, { "Two", 2 }, { "Three", 3 }, };
+			d.Letters = new Dictionary<string, object> { { "A", "a" }, { "B", "b" }, { "C", "c" }, };
+			d.Object = new { Prop1 = "a", Prop2 = "b", Prop3 = "c", };
+
+			return new object[] {
+                d,
+            };
+		}
+
         [Test]
         public void Test_Dictionary()
         {
@@ -53,5 +66,22 @@ namespace SmartFormat.Tests
             formatter.Test(formats, args, expected);
         }
 
+        [Test]
+        public void Test_Dynamic()
+        {
+            var formatter = Smart.CreateDefaultSmartFormat();
+            formatter.AddExtensions(new DictionarySource(formatter));
+
+            var formats = new string[] {
+                "Chained: {0.Numbers.One} {Numbers.Two} {Letters.A} {Object.Prop1} ",
+                "Nested: {0:{Numbers:{One} {Two}} } {Letters:{A}} {Object:{Prop1}} ", // Due to double-brace escaping, the spacing in this nested format is irregular
+            };
+            var expected = new string[] {
+                "Chained: 1 2 a a ",
+                "Nested: 1 2  a a ",
+            };
+			var args = (object[])GetDynamicArgs();
+            formatter.Test(formats, args, expected);
+        }
     }
 }
