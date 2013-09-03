59ce644a98d54cf20dd54b47c4a8b79fdfbc701a
Bernhard Millauer <bernhard.millauer@update.com>
2013-09-03 13:41:23 +0200

    The DictionarySource and ReflectionSource now supports the CaseSensitivity settings;

--- src/SmartFormat/Core/Settings/SmartSettings.cs
+++ src/SmartFormat/Core/Settings/SmartSettings.cs
@@ -1,4 +1,8 @@
-﻿namespace SmartFormat.Core.Settings
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SmartFormat.Core.Settings
 {
 	public class SmartSettings
 	{
@@ -8,5 +12,33 @@
 		}
 
 		public CaseSensitivityType CaseSensitivity { get; set; }
+
+		internal IEqualityComparer<string> GetCaseSensitivityComparer()
+		{
+			{
+				switch (CaseSensitivity)
+				{
+					case CaseSensitivityType.CaseSensitiv:
+						return StringComparer.CurrentCulture;
+					case CaseSensitivityType.CaseInsensitiv:
+						return StringComparer.CurrentCultureIgnoreCase;
+					default:
+						throw new InvalidOperationException(string.Format("The case sensitivity type [{0}] is unknown.", CaseSensitivity));
+				}
+			}
+		}
+
+		internal BindingFlags GetCaseSensitivityBindingFlag()
+		{
+			switch (CaseSensitivity)
+			{
+				case CaseSensitivityType.CaseSensitiv:
+					return 0;
+				case CaseSensitivityType.CaseInsensitiv:
+					return BindingFlags.IgnoreCase;
+				default:
+					throw new InvalidOperationException(string.Format("The case sensitivity type [{0}] is unknown.", CaseSensitivity));
+			}
+		}
 	}
 }
--- src/SmartFormat/Extensions/DictionarySource.cs
+++ src/SmartFormat/Extensions/DictionarySource.cs
@@ -1,42 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-
 using SmartFormat.Core.Extensions;
 using SmartFormat.Core.Parsing;
 
 namespace SmartFormat.Extensions
 {
-    public class DictionarySource : ISource
-    {
-        public DictionarySource(SmartFormatter formatter)
-        {
-            // Add some special info to the parser:
-            formatter.Parser.AddAlphanumericSelectors(); // (A-Z + a-z)
-            formatter.Parser.AddAdditionalSelectorChars("_");
-            formatter.Parser.AddOperators(".");
-        }
+	public class DictionarySource : ISource
+	{
+		public DictionarySource(SmartFormatter formatter)
+		{
+			// Add some special info to the parser:
+			formatter.Parser.AddAlphanumericSelectors(); // (A-Z + a-z)
+			formatter.Parser.AddAdditionalSelectorChars("_");
+			formatter.Parser.AddOperators(".");
+		}
 
-        public void EvaluateSelector(object current, Selector selector, ref bool handled, ref object result, FormatDetails formatDetails)
-        {
-            // See if current is a IDictionary and contains the selector:
-            var rawDict = current as IDictionary;
+		public void EvaluateSelector(object current, Selector selector, ref bool handled, ref object result, FormatDetails formatDetails)
+		{
+			// See if current is a IDictionary and contains the selector:
+			var rawDict = current as IDictionary;
 
-            if (rawDict != null && rawDict.Keys.Cast<string>().Contains(selector.Text))
-            {
-                result = rawDict[selector.Text];
-                handled = true;
-	            return;
-            }
+			if (rawDict != null && rawDict.Keys.Cast<string>().Contains(selector.Text, Smart.Settings.GetCaseSensitivityComparer()))
+			{
+				result = rawDict[selector.Text];
+				handled = true;
+				return;
+			}
 
 			// this check is for dynamics and generic dictionaries
 			var dict = current as IDictionary<string, object>;
 
-			if (dict != null && dict.ContainsKey(selector.Text))
+			if (dict != null && dict.Keys.Contains(selector.Text, Smart.Settings.GetCaseSensitivityComparer()))
 			{
 				result = dict[selector.Text];
 				handled = true;
 			}
-        }
-    }
+		}
+	}
 }
