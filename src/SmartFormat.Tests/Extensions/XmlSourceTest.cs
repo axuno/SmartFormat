332428d6e80c0c643a3b8ad715e0a237e7743397
Taras B <taras.budurovych@gmail.com>
2014-05-04 19:58:13 +0300

    Added Xml Source as an extension.

---
+++ src/SmartFormat.Tests/Extensions/XmlSourceTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Linq;
+using NUnit.Framework;
+using SmartFormat.Extensions;
+
+namespace SmartFormat.Tests
+{
+    [TestFixture]
+    public class XmlSourceTest
+    {
+
+        [Test]
+        public void Format_ValidXml_Formatted()
+        {
+            // arrange
+            const string xmlStr = "<root>" +
+                                  "<FirstName>Joe</FirstName><LastName>Doe</LastName>" +
+                                  "</root>";
+            var xmlEl = XElement.Parse(xmlStr);
+            Smart.Default.AddExtensions(new XmlSource(Smart.Default));
+            // act
+            var res = Smart.Format("Mr. {FirstName} {LastName}", xmlEl);
+            // assert
+            Assert.AreEqual("Mr. Joe Doe", res);
+        }
+    }
+}
---
+++ src/SmartFormat/Extensions/XmlSource.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using SmartFormat.Core.Extensions;
+using SmartFormat.Core.Parsing;
+
+namespace SmartFormat.Extensions
+{
+	public class XmlSource : ISource
+	{
+		public XmlSource(SmartFormatter formatter)
+		{
+			// Add some special info to the parser:
+			formatter.Parser.AddAlphanumericSelectors(); // (A-Z + a-z)
+			formatter.Parser.AddAdditionalSelectorChars("_");
+			formatter.Parser.AddOperators(".");
+		}
+
+		public void EvaluateSelector(object current, Selector selector, ref bool handled,
+            ref object result, FormatDetails formatDetails)
+		{
+		    var element = current as XElement;
+		    if (element != null)
+		    {
+		        foreach (var el in element.Elements())
+		        {
+		            if (el.Name.LocalName.Equals(selector.Text, Smart.Settings.GetCaseSensitivityComparison()))
+		            {
+		                result = el.Value;
+		                handled = true;
+		                return;
+		            }
+		        }
+		    }
+
+		}
+	}
+}
