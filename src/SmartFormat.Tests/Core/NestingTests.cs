2a1d39bd284ee8b8779091f165905693738b5257
Scott Rippey <scott.william.rippey@gmail.com>
2014-08-11 00:09:13 -0700

    [Nesting] Added some Nesting tests, to demonstrate the current functionality

---
+++ src/SmartFormat.Tests/Core/NestingTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartFormat.Tests.Core
+{
+    [TestFixture]
+    public class NestingTests
+    {
+        private Address address = new Address("123 Main St", "San Diego", "CA", "92000");
+
+        [Test]
+        public void Simple_Nesting()
+        {
+            var actual = Smart.Format("{Address:{StreetAddress}, {City}, {StateAbbreviation} {Zip}}", new { Address = address });
+            Assert.AreEqual("123 Main St, San Diego, CA 92000", actual);
+        }
+        [Test]
+        public void Nesting_With_Root()
+        {
+            var actual = Smart.Format("{Address1:{City}, {0.Address2.City}}", new {
+                Address1 = address,
+                Address2 = address
+            });
+            Assert.AreEqual("San Diego, San Diego", actual);
+        }
+    }
+}
