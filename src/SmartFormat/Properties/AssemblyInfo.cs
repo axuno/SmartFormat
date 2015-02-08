894827bbdd23913ae9c2f4235369c45a382c5ae9
Scott Rippey <scott.william.rippey@gmail.com>
2015-02-08 00:15:24 -0800

    [Branch] Created v1.3 branch

--- src/SmartFormat/Properties/AssemblyInfo.cs
+++ src/SmartFormat/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SmartFormat")]
 [assembly: AssemblyCopyright("Copyright 2015 Scott Rippey")]
 [assembly: AssemblyDescription("A string composition library written in C# that can format data into a string with a minimal, intuitive syntax. It uses extensions to provide named placeholders, pluralization, gender conjugation, and time and list formatting.")]
-[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
