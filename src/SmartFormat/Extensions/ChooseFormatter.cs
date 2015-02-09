e28ba9ac86291179eebed961b8dac393e1b2aa25
Scott Rippey <Scott.William.Rippey@gmail.com>
2015-02-09 15:21:43 -0800

    [Extension] [Choose] Started the Choose formatter

---
+++ src/SmartFormat/Extensions/ChooseFormatter.cs
@@ -0,0 +1,17 @@
+﻿using SmartFormat.Core.Extensions;
+
+namespace SmartFormat.Extensions
+{
+	public class ChooseFormatter : IFormatter
+	{
+		private string[] names = { "choose", "c" };
+		public string[] Names { get { return names; } set { names = value; } }
+
+		public void EvaluateFormat(IFormattingInfo formattingInfo)
+		{
+			var options = formattingInfo.FormatterOptions.Split(',');
+
+
+		}
+	}
+}
