143ea87166f4f7e00fad19121164138668ec8e5a
Scott Rippey <scott.william.rippey@gmail.com>
2015-02-11 00:11:04 -0800

    [Optimization] Lazy-loaded unused property.  Not sure if it should just be removed.

--- src/SmartFormat/Core/Formatting/FormatCache.cs
+++ src/SmartFormat/Core/Formatting/FormatCache.cs
@@ -12,17 +12,18 @@ namespace SmartFormat.Core.Formatting
 		public FormatCache(Format format)
 		{
 			this.Format = format;
-			this.CachedObjects = new Dictionary<string, object>();
 		}
 		/// <summary>
 		/// Caches the parsed format.
 		/// </summary>
 		public Format Format { get; private set; }
+
+		private Dictionary<string, object> cachedObjects;
 		/// <summary>
 		/// Storage for any misc objects.
 		/// This can be used by extensions that want to cache data,
 		/// such as reflection information.
 		/// </summary>
-		public Dictionary<string, object> CachedObjects { get; private set; }
+		public Dictionary<string, object> CachedObjects { get { return cachedObjects ?? (cachedObjects = new Dictionary<string, object>()); } }
 	}
 }
