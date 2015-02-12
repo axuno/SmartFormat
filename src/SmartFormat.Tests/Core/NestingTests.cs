a7f81f3057686174fe205d5e12ae6a933dd147be
Scott Rippey <scott.william.rippey@gmail.com>
2015-02-11 23:05:50 -0800

    [Nested Scopes] Refactored test

--- src/SmartFormat.Tests/Core/NestingTests.cs
+++ src/SmartFormat.Tests/Core/NestingTests.cs
@@ -1,26 +1,34 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Drawing.Text;
+using NUnit.Framework;
 
 namespace SmartFormat.Tests.Core
 {
 	[TestFixture]
 	public class NestingTests
 	{
-		private Address address = new Address("123 Main St", "San Diego", "CA", "92000");
+		public NestingTests()
+		{
+			var data = new {
+				Address1 = new Address("123 Main St", "San Diego", "CA", "92000"),
+				Address2 = new Address("987 Second St", "Los Angeles", "CA", "90210"),
+				Person1 = new Person("Dwight Schrute", Gender.Male, new DateTime(), ""),
+				Person2 = new Person("Michael Scott", Gender.Male, new DateTime(), ""),
+			};
+			data.Person1.Address = data.Address1;
+			data.Person2.Address = data.Address2;
+
+			this.data = data;
+		}
+		private object data;
 
 		[Test]
-		public void Simple_Nesting()
+		[TestCase("{Address1:{StreetAddress}, {City}, {StateAbbreviation} {Zip}}", "123 Main St, San Diego, CA 92000")]
+		[TestCase("{Address1:{City}, {0.Address2.City}}", "San Diego, Los Angeles")]
+		public void Nesting_can_access_outer_scope_via_number(string format, string expectedOutput)
 		{
-			var actual = Smart.Format("{Address:{StreetAddress}, {City}, {StateAbbreviation} {Zip}}", new { Address = address });
-			Assert.AreEqual("123 Main St, San Diego, CA 92000", actual);
-		}
-		[Test]
-		public void Nesting_With_Root()
-		{
-			var actual = Smart.Format("{Address1:{City}, {0.Address2.City}}", new {
-				Address1 = address,
-				Address2 = address
-			});
-			Assert.AreEqual("San Diego, San Diego", actual);
+			var actual = Smart.Format(format, data);
+			Assert.AreEqual(expectedOutput, actual);
 		}
 	}
 }
