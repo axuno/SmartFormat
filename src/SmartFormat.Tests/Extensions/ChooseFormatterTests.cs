81ff1162bb5c651c2ab5e9dc1847dc93c4c737bd
Scott Rippey <Scott.William.Rippey@gmail.com>
2015-03-04 14:53:55 -0800

    [Tests] Added boolean tests to "choose" formatter

--- src/SmartFormat.Tests/Extensions/ChooseFormatterTests.cs
+++ src/SmartFormat.Tests/Extensions/ChooseFormatterTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using SmartFormat.Core.Formatting;
 
 namespace SmartFormat.Tests.Extensions
@@ -22,8 +23,21 @@ namespace SmartFormat.Tests.Extensions
 		[TestCase("{0:choose(A|B|C):Alpha|Bravo|Charlie}", "A", "Alpha")]
 		[TestCase("{0:choose(A|B|C):Alpha|Bravo|Charlie}", "B", "Bravo")]
 		[TestCase("{0:choose(A|B|C):Alpha|Bravo|Charlie}", "C", "Charlie")]
-		[Test]
-		public void Choose_should_work_with_numbers_and_strings(string format, object arg0, string expectedResult)
+
+		[TestCase("{0:choose(True|False):yep|nope}", true, "yep")]
+		[TestCase("{0:choose(True|False):yep|nope}", false, "nope")]
+		public void Choose_should_work_with_numbers_strings_and_booleans(string format, object arg0, string expectedResult)
+		{
+			Assert.AreEqual(expectedResult, Smart.Format(format, arg0));
+		}
+
+		[TestCase("{0:choose(true|True):one|two|default}", true, "two")]
+		[TestCase("{0:choose(true|TRUE):one|two|default}", true, "default")]
+		[TestCase("{0:choose(string|String):one|two|default}", "String", "two")]
+		[TestCase("{0:choose(string|STRING):one|two|default}", "String", "default")]
+		[TestCase("{0:choose(ignore|Ignore):one|two|default}", SmartFormat.Core.Settings.ErrorAction.Ignore, "two")]
+		[TestCase("{0:choose(ignore|IGNORE):one|two|default}", SmartFormat.Core.Settings.ErrorAction.Ignore, "default")]
+		public void Choose_should_be_case_sensitive(string format, object arg0, string expectedResult)
 		{
 			Assert.AreEqual(expectedResult, Smart.Format(format, arg0));
 		}
@@ -34,8 +48,8 @@ namespace SmartFormat.Tests.Extensions
 		[TestCase("{0:choose(1|2|3):one|two|three|default}", 4, "default")]
 		[TestCase("{0:choose(1|2|3):one|two|three|default}", 99, "default")]
 		[TestCase("{0:choose(1|2|3):one|two|three|default}", null, "default")]
+		[TestCase("{0:choose(1|2|3):one|two|three|default}", true, "default")]
 		[TestCase("{0:choose(1|2|3):one|two|three|default}", "whatever", "default")]
-		[Test]
 		public void Choose_should_default_to_the_last_item(string format, object arg0, string expectedResult)
 		{
 			Assert.AreEqual(expectedResult, Smart.Format(format, arg0));
@@ -45,7 +59,6 @@ namespace SmartFormat.Tests.Extensions
 		[TestCase("{0:choose(Male|Female):man|woman}", Gender.Female, "woman")]
 		[TestCase("{0:choose(Male):man|woman}", Gender.Male, "man")]
 		[TestCase("{0:choose(Male):man|woman}", Gender.Female, "woman")]
-		[Test]
 		public void Choose_should_work_with_enums(string format, object arg0, string expectedResult)
 		{
 			Assert.AreEqual(expectedResult, Smart.Format(format, arg0));
@@ -56,7 +69,6 @@ namespace SmartFormat.Tests.Extensions
 		[TestCase("{0:choose(null|5):nothing|five|{} }", null, "nothing")]
 		[TestCase("{0:choose(null|5):nothing|five|{} }", 5, "five")]
 		[TestCase("{0:choose(null|5):nothing|five|{} }", 6, "6 ")]
-		[Test]
 		public void Choose_has_a_special_case_for_null(string format, object arg0, string expectedResult)
 		{
 			Assert.AreEqual(expectedResult, Smart.Format(format, arg0));
@@ -64,7 +76,6 @@ namespace SmartFormat.Tests.Extensions
 
 		[TestCase("{0:choose(1|2):1|2}", 99)]
 		[TestCase("{0:choose(1):1}", 99)]
-		[Test]
 		[ExpectedException(typeof(FormattingException))]
 		public void Choose_throws_when_choice_is_invalid(string format, object arg0)
 		{
@@ -77,7 +88,6 @@ namespace SmartFormat.Tests.Extensions
 		// Too many choices:
 		[TestCase("{0:choose(1):1|2|3}", 1)]
 		[TestCase("{0:choose(1|2):1|2|3|4}", 1)]
-		[Test]
 		[ExpectedException(typeof(FormattingException))]
 		public void Choose_throws_when_choices_are_too_few_or_too_many(string format, object arg0)
 		{
