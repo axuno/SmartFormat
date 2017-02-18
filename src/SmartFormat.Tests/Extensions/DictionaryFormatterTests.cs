aa1167918058cab5c8e4afc6e6ea1476612b81e2
axunonb <nb@axuno.net>
2017-02-18 23:26:33 +0100

    Improved Dictionary dot notation tests

--- src/SmartFormat.Tests/Extensions/DictionaryFormatterTests.cs
+++ src/SmartFormat.Tests/Extensions/DictionaryFormatterTests.cs
@@ -53,17 +53,20 @@ namespace SmartFormat.Tests.Extensions
 		{
 			var formatter = Smart.CreateDefaultSmartFormat();
 			formatter.AddExtensions(new DictionarySource(formatter));
+            formatter.Parser.UseAlternativeEscapeChar(); // curly braces MUST be escaped with \{ and \} instead of {{ and }} for this complex test
 
-			var formats = new string[] {
-				"Chained: {0.Numbers.One} {Numbers.Two} {Letters.A} {Object.Prop1} ",
-				"Nested: {0:{Numbers:{One} {Two}} } {Letters:{A}} {Object:{Prop1}} ", // Due to double-brace escaping, the spacing in this nested format is irregular
+            var formats = new string[] {
+				"Chained: {0.Numbers.One} {Numbers.Two} {Letters.A} {Object.Prop1}",
+				"Nested: {0:{Numbers:{One} {Two}}} {Letters:{A}} {Object:{Prop1}}" 
 			};
 			var expected = new string[] {
-				"Chained: 1 2 a a ",
-				"Nested: 1 2  a a ",
+				"Chained: 1 2 a a",
+				"Nested: 1 2 a a"
 			};
 			var args = GetArgs();
 			formatter.Test(formats, args, expected);
+
+            formatter.Parser.UseBraceEscaping(); // reset to string.Format brace escaping
 		}
 
 		[Test]
@@ -71,18 +74,21 @@ namespace SmartFormat.Tests.Extensions
 		{
 			var formatter = Smart.CreateDefaultSmartFormat();
 			formatter.AddExtensions(new DictionarySource(formatter));
+            formatter.Parser.UseAlternativeEscapeChar(); // curly braces MUST be escaped with \{ and \} instead of {{ and }} for this complex test
 
-			var formats = new string[] {
-				"Chained: {0.Numbers.One} {Numbers.Two} {Letters.A} {Object.Prop1} {Raw.X} ",
-				"Nested: {0:{Numbers:{One} {Two}} } {Letters:{A}} {Object:{Prop1}} {Raw:{X}} ", // Due to double-brace escaping, the spacing in this nested format is irregular
+            var formats = new string[] {
+				"Chained: {0.Numbers.One} {Numbers.Two} {Letters.A} {Object.Prop1} {Raw.X}",
+				"Nested: {0:{Numbers:{One} {Two}}} {Letters:{A}} {Object:{Prop1}} {Raw:{X}}"
 			};
 			var expected = new string[] {
-				"Chained: 1 2 a a z ",
-				"Nested: 1 2  a a z ",
+				"Chained: 1 2 a a z",
+				"Nested: 1 2 a a z"
 			};
 			var args = (object[])GetDynamicArgs();
 			formatter.Test(formats, args, expected);
-		}
+
+            formatter.Parser.UseBraceEscaping(); // reset to string.Format brace escaping
+        }
 
 		[Test]
 		public void Test_Dynamic_CaseInsensitive()
@@ -90,17 +96,87 @@ namespace SmartFormat.Tests.Extensions
 			var formatter = Smart.CreateDefaultSmartFormat();
 			formatter.Settings.CaseSensitivity = CaseSensitivityType.CaseInsensitive;
 			formatter.AddExtensions(new DictionarySource(formatter));
+            formatter.Parser.UseAlternativeEscapeChar(); // curly braces MUST be escaped with \{ and \} instead of {{ and }} for this complex test
 
-			var formats = new string[] {
-				"Chained: {0.Numbers.One} {Numbers.Two} {Letters.A} {Object.Prop1} {Raw.x} ",
-				"Nested: {0:{Numbers:{One} {Two}} } {Letters:{A}} {Object:{Prop1}} {Raw:{x}} ", // Due to double-brace escaping, the spacing in this nested format is irregular
+            var formats = new string[] {
+				"Chained: {0.Numbers.One} {Numbers.Two} {Letters.A} {Object.Prop1} {Raw.x}",
+				"Nested: {0:{Numbers:{One} {Two}}} {Letters:{A}} {Object:{Prop1}} {Raw:{x}}"
 			};
 			var expected = new string[] {
-				"Chained: 1 2 a a z ",
-				"Nested: 1 2  a a z ",
+				"Chained: 1 2 a a z",
+				"Nested: 1 2 a a z"
 			};
 			var args = (object[])GetDynamicArgs();
 			formatter.Test(formats, args, expected);
-		}
-	}
+
+            formatter.Parser.UseBraceEscaping(); // reset to string.Format brace escaping
+        }
+
+	    [Test]
+	    public void Dictionary_Dot_Notation()
+	    {
+            // Process properties of a class instance type-safe and without the need for reflection
+            // and with dot notation for dictionaries
+
+            var addr = new Address();
+
+	        const string format = "Address: {City.ZipCode} {City.Name}, {City.AreaCode}\n" +
+	                              "Name: {Person.FirstName} {Person.LastName}";
+
+            var expected = $"Address: {addr.City.ZipCode} {addr.City.Name}, {addr.City.AreaCode}\n" +
+                         $"Name: {addr.Person.FirstName} {addr.Person.LastName}";
+
+            var formatter = Smart.CreateDefaultSmartFormat();
+	        var result = formatter.Format(format, addr.ToDictionary());
+
+            Assert.AreEqual(expected, result);
+	    }
+
+        public class Address
+        {
+            public CityDetails City { get; set; } = new CityDetails();
+            public PersonDetails Person { get; set; } = new PersonDetails();
+
+            public Dictionary<string, object> ToDictionary()
+            {
+                var d = new Dictionary<string, object>
+                {
+                    { nameof(City), City.ToDictionary() },
+                    { nameof(Person), Person.ToDictionary() }
+                };
+                return d;
+            }
+
+            public class CityDetails
+            {
+                public string Name { get; set; } = "New York";
+                public string ZipCode { get; set; } = "00501";
+                public string AreaCode { get; set; } = "631";
+
+                public Dictionary<string, string> ToDictionary()
+                {
+                    return new Dictionary<string, string>
+                    {
+                        {nameof(Name), Name},
+                        {nameof(ZipCode), ZipCode},
+                        {nameof(AreaCode), AreaCode}
+                    };
+                }
+            }
+
+            public class PersonDetails
+            {
+                public string FirstName { get; set; } = "John";
+                public string LastName { get; set; } = "Doe";
+                public Dictionary<string, string> ToDictionary()
+                {
+                    return new Dictionary<string, string>
+                    {
+                        {nameof(FirstName), FirstName},
+                        {nameof(LastName), LastName}
+                    };
+                }
+            }
+        }
+    }
 }
