35c82f78fc695abf078db3891127cfa447cb95ea
axunonb <nb@axuno.net>
2018-07-15 00:24:12 +0200

    Added SubStringFormatter, closes #80

---
+++ src/SmartFormat.Tests/Extensions/SubStringFormatterTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using SmartFormat.Extensions;
+
+namespace SmartFormat.Tests.Extensions
+{
+    [TestFixture]
+    public class SubStringFormatterTests
+    {
+        private List<object> _people;
+        private SmartFormatter _smart;
+
+        public SubStringFormatterTests()
+        {
+            _smart = Smart.CreateDefaultSmartFormat();
+            if (_smart.FormatterExtensions.FirstOrDefault(fmt => fmt.Names.Contains("substr")) == null)
+            {
+                _smart.FormatterExtensions.Add(new SubStringFormatter());
+            }
+
+            _people = new List<object>
+                {new {Name = "Long John", City = "New York"}, new {Name = "Short Mary", City = "Massachusetts"},};
+        }
+
+
+        [Test]
+        public void NoParameters()
+        {
+            Assert.AreEqual("No parentheses: Long John", _smart.Format("No parentheses: {Name:substr}", _people.First()));
+            Assert.AreEqual("No parameters: ", _smart.Format("No parameters: {Name:substr()}", _people.First()));
+            Assert.AreEqual("Only delimiter: ", _smart.Format("Only delimiter: {Name:substr(,)}", _people.First()));
+        }
+
+        [Test]
+        public void StartPositionLongerThanString()
+        {
+            Assert.AreEqual(string.Empty, _smart.Format("{Name:substr(999)}", _people.First()));
+        }
+
+        [Test]
+        public void StartPositionAndLengthLongerThanString()
+        {
+            Assert.AreEqual(string.Empty, _smart.Format("{Name:substr(999,1)}", _people.First()));
+        }
+
+        [Test]
+        public void OnlyPositiveStartPosition()
+        {
+            Assert.AreEqual("John", _smart.Format("{Name:substr(5)}", _people.First()));
+        }
+
+        [Test]
+        public void StartPositionAndPositiveLength()
+        {
+            Assert.AreEqual("New", _smart.Format("{City:substr(0,3)}", _people.First()));
+        }
+
+        [Test]
+        public void OnlyNegativeStartPosition()
+        {
+            Assert.AreEqual("John", _smart.Format("{Name:substr(-4)}", _people.First()));
+        }
+
+        [Test]
+        public void NegativeStartPositionAndPositiveLength()
+        {
+            Assert.AreEqual("Jo", _smart.Format("{Name:substr(-4, 2)}", _people.First()));
+        }
+
+        [Test]
+        public void NegativeStartPositionAndNegativeLength()
+        {
+            Assert.AreEqual("Joh", _smart.Format("{Name:substr(-4, -1)}", _people.First()));
+        }
+
+        [Test]
+        public void DataItemIsNull()
+        {
+            // null values won't be processed by SubStringFormatter, but by DefaultFormatter
+            Assert.AreEqual(string.Empty, _smart.Format("{Name:substr(0,3)}", new Dictionary<string, string> { { "Name", null } }.First()));
+        }
+    }
+}
---
+++ src/SmartFormat/Extensions/SubStringFormatter.cs
@@ -0,0 +1,51 @@
+﻿using SmartFormat.Core.Extensions;
+
+namespace SmartFormat.Extensions
+{
+    /// <summary>
+    /// Formatter to access part of a string.
+    /// </summary>
+    public class SubStringFormatter : IFormatter
+    {
+        /// <summary>
+        /// Formatter names
+        /// </summary>
+        public string[] Names { get; set; } = {"substr"};
+
+        /// <summary>
+        /// The delimiter to separate parameters, defaults to comma.
+        /// </summary>
+        public char ParameterDelimiter { get; set; } = ',';
+
+        /// <summary>
+        /// Tries to process the given <see cref="IFormattingInfo"/>.
+        /// </summary>
+        /// <param name="formattingInfo">Returns true if processed, otherwise false.</param>
+        /// <returns></returns>
+        public bool TryEvaluateFormat(IFormattingInfo formattingInfo)
+        {
+            if (formattingInfo.FormatterOptions == string.Empty) return false;
+            var parameters = formattingInfo.FormatterOptions.Split(ParameterDelimiter);
+
+            var currentValue = formattingInfo.CurrentValue?.ToString() ?? string.Empty;
+
+            var startPos = int.Parse(parameters[0]);
+            var length = parameters.Length > 1 ? int.Parse(parameters[1]) : 0;
+            if (startPos < 0)
+                startPos = currentValue.Length + startPos;
+            if (startPos > currentValue.Length)
+                startPos = currentValue.Length;
+            if (length < 0)
+                length = currentValue.Length - startPos + length;
+            if (startPos + length > currentValue.Length)
+                length = 0;
+            var substring = parameters.Length > 1
+                ? currentValue.Substring(startPos, length)
+                : currentValue.Substring(startPos);
+
+            formattingInfo.Write(substring);
+
+            return true;
+        }
+    }
+}
