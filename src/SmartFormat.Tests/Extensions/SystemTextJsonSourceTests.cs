c9150156e1e0465e92bc9db08998fbe5c5281610
axunonb <nb@axuno.net>
2021-06-20 08:28:31 +0200

    Separated JsonSource to NewtonsoftJsonSource and SystemTextJsonSource

--- src/SmartFormat.Tests/Extensions/SystemTextJsonSourceTests.cs
+++ src/SmartFormat.Tests/Extensions/SystemTextJsonSourceTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Globalization;
-using Newtonsoft.Json.Linq;
 using System.Text.Json;
 using NUnit.Framework;
 using SmartFormat.Core.Extensions;
@@ -11,7 +10,7 @@ using SmartFormat.Extensions;
 namespace SmartFormat.Tests.Extensions
 {
     [TestFixture]
-    public class JsonSourceTests
+    public class SystemTextJsonSourceTests
     {
         private const string JsonOneLevel = @"{'Name': 'Doe'}";
         private const string JsonTwoLevel = @"{'Name': {'First': 'Joe'}}";
@@ -49,97 +48,18 @@ namespace SmartFormat.Tests.Extensions
     }
   ]
 }";
-
         private SmartFormatter GetFormatterWithJsonSource()
         {
             var smart = new SmartFormatter();
-            // JsonSource MUST be registered before ReflectionSource (which is not required here)
-            smart.AddExtensions(new ISource[] { new ListFormatter(smart), new DefaultSource(smart), new JsonSource(smart) });
+            // SystemTextJsonSource MUST be registered before ReflectionSource (which is not required here)
+            // We also need the ListFormatter to process arrays
+            smart.AddExtensions(new ISource[] { new ListFormatter(smart), new DefaultSource(smart), new SystemTextJsonSource(smart) });
             smart.AddExtensions(new IFormatter[] {new ListFormatter(smart), new DefaultFormatter()});
             return smart;
         }
 
-        #region *** NewtonSoftJson ***
-
         [Test]
-        public void NS_Format_Null_Json()
-        {
-            var jObject = JObject.Parse(JsonNull);
-            var result = GetFormatterWithJsonSource().Format("{Name}", jObject);
-            Assert.AreEqual("", result);
-        }
-
-        [Test]
-        public void NS_Format_OneLevel_Json()
-        {
-            var jObject = JObject.Parse(JsonOneLevel);
-            var result = GetFormatterWithJsonSource().Format("{Name}", jObject);
-            Assert.AreEqual("Doe", result);
-        }
-
-        [Test]
-        public void NS_Format_TwoLevel_Json()
-        {
-            var jObject = JObject.Parse(JsonTwoLevel);
-            var result = GetFormatterWithJsonSource().Format("{Name.First}", jObject);
-            Assert.AreEqual("Joe", result);
-        }
-
-        [Test]
-        public void NS_Format_TwoLevel_Nullable_Json()
-        {
-            var jObject = JObject.Parse(JsonNull);
-            var result = GetFormatterWithJsonSource().Format("{Name?.First}", jObject);
-            Assert.AreEqual("", result);
-        }
-
-        [Test]
-        public void NS_Format_Complex_Json()
-        {
-            var jObject = JObject.Parse(JsonComplex);
-            var smart = GetFormatterWithJsonSource();
-            smart.Settings.CaseSensitivity = CaseSensitivityType.CaseSensitive;
-            Assert.Multiple(() =>
-            {
-                Assert.AreEqual("50.00", smart.Format(CultureInfo.InvariantCulture, "{Manufacturers[0].Products[0].Price:0.00}", jObject));
-                Assert.AreEqual("True", smart.Format(CultureInfo.InvariantCulture, "{Manufacturers[1].Products[0].OnStock}", jObject));
-                Assert.AreEqual("False", smart.Format(CultureInfo.InvariantCulture, "{Manufacturers[1].Products[1].OnStock}", jObject));
-            });
-        }
-
-        [Test]
-        public void NS_Format_Complex_Json_CaseInsensitive()
-        {
-            var jObject = JObject.Parse(JsonComplex);
-            var smart = GetFormatterWithJsonSource();
-            smart.Settings.CaseSensitivity = CaseSensitivityType.CaseInsensitive;
-            var result = smart.Format(CultureInfo.InvariantCulture, "{MaNuFaCtUrErS[0].PrOdUcTs[0].PrIcE:0.00}", jObject);
-            Assert.AreEqual("50.00", result);
-        }
-
-        [Test]
-        public void NS_Format_List_Json()
-        {
-            var jObject = JObject.Parse(JsonComplex);
-            var result = GetFormatterWithJsonSource().Format("{Stores:list:{}|, |, and }", jObject);
-            Assert.AreEqual("Lambton Quay, and Willis Street", result);
-        }
-
-        [Test]
-        public void NS_Format_Exception_Json()
-        {
-            var smart = GetFormatterWithJsonSource();
-            smart.Settings.Formatter.ErrorAction = FormatErrorAction.ThrowError;
-            var jObject = JObject.Parse(JsonOneLevel);
-            Assert.Throws<FormattingException>(() => smart.Format("{Dummy}", jObject));
-        }
-
-        #endregion
-
-        #region *** System.Text.Json ***
-
-        [Test]
-        public void ST_Format_Null_Json()
+        public void Format_Null_Json()
         {
             var jObject = JsonDocument.Parse(JsonNull.Replace("'", "\"")).RootElement;
             var result = GetFormatterWithJsonSource().Format("{Name}", jObject);
@@ -147,7 +67,7 @@ namespace SmartFormat.Tests.Extensions
         }
 
         [Test]
-        public void ST_Format_OneLevel_Json()
+        public void Format_OneLevel_Json()
         {
             var jObject = JsonDocument.Parse(JsonOneLevel.Replace("'", "\"")).RootElement;
             var result = GetFormatterWithJsonSource().Format("{Name}", jObject);
@@ -155,7 +75,7 @@ namespace SmartFormat.Tests.Extensions
         }
 
         [Test]
-        public void ST_Format_TwoLevel_Json()
+        public void Format_TwoLevel_Json()
         {
             var jObject = JsonDocument.Parse(JsonTwoLevel.Replace("'", "\"")).RootElement;
             var result = GetFormatterWithJsonSource().Format("{Name.First}", jObject);
@@ -163,7 +83,7 @@ namespace SmartFormat.Tests.Extensions
         }
 
         [Test]
-        public void ST_Format_TwoLevel_Nullable_Json()
+        public void Format_TwoLevel_Nullable_Json()
         {
             var jObject = JsonDocument.Parse(JsonNull.Replace("'", "\"")).RootElement;
             var result = GetFormatterWithJsonSource().Format("{Name?.First}", jObject);
@@ -171,7 +91,7 @@ namespace SmartFormat.Tests.Extensions
         }
 
         [Test]
-        public void ST_Format_Complex_Json()
+        public void Format_Complex_Json()
         {
             var jObject = JsonDocument.Parse(JsonComplex.Replace("'", "\"")).RootElement;
             var savedSetting = Smart.Default.Settings.CaseSensitivity;
@@ -187,7 +107,7 @@ namespace SmartFormat.Tests.Extensions
         }
 
         [Test]
-        public void ST_Format_Complex_Json_CaseInsensitive()
+        public void Format_Complex_Json_CaseInsensitive()
         {
             var jObject = JsonDocument.Parse(JsonComplex.Replace("'", "\"")).RootElement;
             var smart = GetFormatterWithJsonSource();
@@ -197,7 +117,7 @@ namespace SmartFormat.Tests.Extensions
         }
 
         [Test]
-        public void ST_Format_List_Json()
+        public void Format_LiJson()
         {
             var jObject = JsonDocument.Parse(JsonComplex.Replace("'", "\"")).RootElement;
             var result = GetFormatterWithJsonSource().Format("{Stores:list:{}|, |, and }", jObject);
@@ -205,14 +125,12 @@ namespace SmartFormat.Tests.Extensions
         }
 
         [Test]
-        public void ST_Format_Exception_Json()
+        public void Format_Exception_Json()
         {
             var smart = GetFormatterWithJsonSource();
             smart.Settings.Formatter.ErrorAction = FormatErrorAction.ThrowError;
             var jObject = JsonDocument.Parse(JsonOneLevel.Replace("'", "\"")).RootElement;
             Assert.Throws<FormattingException>(() => smart.Format("{Dummy}", jObject));
         }
-
-        #endregion
     }
 }
---
+++ src/SmartFormat/Extensions/NewtonsoftJsonSource.cs
@@ -0,0 +1,70 @@
+﻿//
+// Copyright (C) axuno gGmbH, Scott Rippey, Bernhard Millauer and other contributors.
+// Licensed under the MIT license.
+//
+
+using System;
+using Newtonsoft.Json.Linq;
+using SmartFormat.Core.Extensions;
+
+namespace SmartFormat.Extensions
+{
+    /// <summary>
+    /// Class to evaluate <see cref="Newtonsoft.Json"/> JSON sources
+    /// of type <see cref="JObject"/> and <see cref="JValue"/>.
+    /// </summary>
+    public class NewtonsoftJsonSource : Source
+    {
+        /// <summary>
+        /// CTOR.
+        /// </summary>
+        /// <param name="formatter"></param>
+        public NewtonsoftJsonSource(SmartFormatter formatter) : base(formatter)
+        {
+        }
+
+        /// <inheritdoc />
+        public override bool TryEvaluateSelector(ISelectorInfo selectorInfo)
+        {
+            // Check for nullable and null value
+            var current = selectorInfo.CurrentValue switch
+            {
+                JObject jsonObject => jsonObject.HasValues ? jsonObject : null,
+                JValue jsonValue => jsonValue.Value,
+                _ => selectorInfo.CurrentValue
+            };
+            
+            if (current is null && HasNullableOperator(selectorInfo))
+            {
+                selectorInfo.Result = null;
+                return true;
+            }
+
+            if (current is null) return false;
+
+            return selectorInfo.CurrentValue switch
+            {
+                // Note: Operators are processed by ListFormatter
+                
+                JObject jObject => TryEvaluateJObject(jObject, selectorInfo),
+                JValue jValue => TryEvaluateJValue(jValue, selectorInfo),
+                _ => false
+            };
+        }
+
+        private static bool TryEvaluateJObject(JObject jsonObject, ISelectorInfo selectorInfo)
+        {
+            var jToken = jsonObject.GetValue(selectorInfo.SelectorText,
+                selectorInfo.FormatDetails.Settings.GetCaseSensitivityComparison());
+
+            selectorInfo.Result = jToken ?? throw new FormatException($"'{selectorInfo.SelectorText}'");
+            return true;
+        }
+
+        private static bool TryEvaluateJValue(JValue jsonValue, ISelectorInfo selectorInfo)
+        {
+            selectorInfo.Result = jsonValue;
+            return true;
+        }
+    }
+}
