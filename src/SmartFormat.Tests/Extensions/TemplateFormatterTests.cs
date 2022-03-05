1af96fbcc668a361bae8d256e8db614c6c2a42e0
axunonb <axunonb@users.noreply.github.com>
2022-03-05 10:18:23 +0100

    TemplateFormatter.Name is now "t" (#256)

--- src/SmartFormat.Tests/Extensions/TemplateFormatterTests.cs
+++ src/SmartFormat.Tests/Extensions/TemplateFormatterTests.cs
@@ -24,7 +24,7 @@ namespace SmartFormat.Tests.Extensions
                 templates.Register("LAST", "{Last.ToUpper}");
             }
 
-            templates.Register("NESTED", "{:template:FIRST} {:template:last}");
+            templates.Register("NESTED", "{:t:FIRST} {:t:last}");
 
             return smart;
         }
@@ -48,7 +48,7 @@ namespace SmartFormat.Tests.Extensions
         public void Undefined_Template_Should_Throw()
         {
             var smart = GetFormatterWithRegisteredTemplates(CaseSensitivityType.CaseInsensitive);;
-            Assert.That(() => smart.Format("{:template:does-not-exist}", new object()), Throws.Exception.TypeOf<FormattingException>());
+            Assert.That(() => smart.Format("{:t:does-not-exist}", new object()), Throws.Exception.TypeOf<FormattingException>());
         }
 
         [Test]
@@ -61,13 +61,13 @@ namespace SmartFormat.Tests.Extensions
         }
 
         [Test]
-        [TestCase("{:template(firstLast)}", "Scott Rippey")]
-        [TestCase("{:template:firstLast}", "Scott Rippey")]
-        [TestCase("{:template():firstLast}", "Scott Rippey")]
-        [TestCase("{:template(firstLast)}", "Scott Rippey")]
-        [TestCase("{:template:firstLast}", "Scott Rippey")]
-        [TestCase("{:template(firstLast):IGNORED}", "Scott Rippey")]
-        [TestCase("{:template:firstLast}", "Scott Rippey")]
+        [TestCase("{:t(firstLast)}", "Scott Rippey")]
+        [TestCase("{:t:firstLast}", "Scott Rippey")]
+        [TestCase("{:t():firstLast}", "Scott Rippey")]
+        [TestCase("{:t(firstLast)}", "Scott Rippey")]
+        [TestCase("{:t:firstLast}", "Scott Rippey")]
+        [TestCase("{:t(firstLast):IGNORED}", "Scott Rippey")]
+        [TestCase("{:t:firstLast}", "Scott Rippey")]
         public void Template_can_be_called_with_options_or_with_formatString(string format, string expected)
         {
             var smart = GetFormatterWithRegisteredTemplates(CaseSensitivityType.CaseInsensitive);
@@ -83,10 +83,10 @@ namespace SmartFormat.Tests.Extensions
         }
 
         [Test]
-        [TestCase("{:template:lastFirst}", "Rippey, Scott")]
-        [TestCase("{:template:FIRST}", "SCOTT")]
-        [TestCase("{:template:last}", "rippey")]
-        [TestCase("{:template:LAST}", "RIPPEY")]
+        [TestCase("{:t:lastFirst}", "Rippey, Scott")]
+        [TestCase("{:t:FIRST}", "SCOTT")]
+        [TestCase("{:t:last}", "rippey")]
+        [TestCase("{:t:LAST}", "RIPPEY")]
         public void Simple_templates_work_as_expected(string format, string expected)
         {
             var smart = GetFormatterWithRegisteredTemplates(CaseSensitivityType.CaseSensitive);;
@@ -102,8 +102,8 @@ namespace SmartFormat.Tests.Extensions
         }
 
         [Test]
-        [TestCase("{:template:FIRST} {:template:last}", "SCOTT rippey")]
-        [TestCase("{:template:firstLast} | {:template:lastFirst}", "Scott Rippey | Rippey, Scott")]
+        [TestCase("{:t:FIRST} {:t:last}", "SCOTT rippey")]
+        [TestCase("{:t:firstLast} | {:t:lastFirst}", "Scott Rippey | Rippey, Scott")]
         public void Multiple_templates_can_be_used(string format, string expected)
         {
             var smart = GetFormatterWithRegisteredTemplates(CaseSensitivityType.CaseInsensitive);;
@@ -118,25 +118,38 @@ namespace SmartFormat.Tests.Extensions
             Assert.AreEqual(expected, actual);
         }
 
-        [Test]
-        [TestCase("{:template:NESTED}", "SCOTT rippey")]
-        public void Templates_can_be_nested(string format, string expected)
+        [TestCase(true, "Dear Mr Doe:")]
+        [TestCase(false, "Hi Joe:")]
+        public void Templates_can_be_nested(bool useFormalSalutation, string expected)
         {
-            var smart = GetFormatterWithRegisteredTemplates(CaseSensitivityType.CaseInsensitive);;
-            
-            var person = new
-            {
-                First = "Scott",
-                Last = "Rippey",
+            var smart = Smart.CreateDefaultSmartFormat();
+            var templates = new TemplateFormatter();
+            smart.AddExtensions(templates);
+
+            // ConditionalFormatter with arg1 
+            // controls the kind of salutation
+            templates.Register("salutation", "{1:cond:{:t:sal_formal}|{:t:sal_informal}}");
+            // Register the formal nested template
+            templates.Register("sal_formal", "Dear Mr {LastName}");
+            // Register the informal nested template
+            templates.Register("sal_informal", "Hi {Nickname}");
+
+            var person = new 
+            { 
+                FirstName = "Joseph", 
+                Nickname = "Joe", 
+                LastName = "Doe" 
             };
 
-            var actual = smart.Format(format, person);
-            Assert.AreEqual(expected, actual);
+            // Use the "master" template with different args
+            var result = smart.Format("{0:t(salutation)}:", person, useFormalSalutation);
+
+            Assert.That(result, Is.EqualTo(expected));
         }
 
         [TestCase("{0:list:{First.ToUpper} {Last}|, }", "JIM Halpert, PAM Beasley, DWIGHT Schrute")]
-        [TestCase("{:{:template:FIRST} {:template:last}|, }", "JIM halpert, PAM beasley, DWIGHT schrute")]
-        [TestCase("{:{:template:NESTED}|, }", "JIM halpert, PAM beasley, DWIGHT schrute")]
+        [TestCase("{:{:t:FIRST} {:t:last}|, }", "JIM halpert, PAM beasley, DWIGHT schrute")]
+        [TestCase("{:{:t:NESTED}|, }", "JIM halpert, PAM beasley, DWIGHT schrute")]
         public void Templates_can_be_reused(string format, string expected)
         {
             var smart = GetFormatterWithRegisteredTemplates(CaseSensitivityType.CaseInsensitive);
@@ -167,9 +180,9 @@ namespace SmartFormat.Tests.Extensions
         }
 
         [Test]
-        [TestCase("{:template:first}")]
-        [TestCase("{:template:firstlast}")]
-        [TestCase("{:template:LaSt}")]
+        [TestCase("{:t:first}")]
+        [TestCase("{:t:firstlast}")]
+        [TestCase("{:t:LaSt}")]
         public void Templates_are_case_sensitive(string format)
         {
             var smart = Smart.CreateDefaultSmartFormat(new SmartSettings {Formatter = new FormatterSettings {ErrorAction = FormatErrorAction.ThrowError}});
@@ -177,14 +190,14 @@ namespace SmartFormat.Tests.Extensions
         }
 
         [Test]
-        [TestCase("{:template:first}", "SCOTT")]
-        [TestCase("{:template:FIRST}", "SCOTT")]
-        [TestCase("{:template:last}", "rippey")]
-        [TestCase("{:template:LAST}", "rippey")]
-        [TestCase("{:template:nested}", "SCOTT rippey")] 
-        [TestCase("{:template:NESTED}", "SCOTT rippey")]
-        [TestCase("{:template:NeStEd}", "SCOTT rippey")]
-        [TestCase("{:template:fIrStLaSt}", "Scott Rippey")]
+        [TestCase("{:t:first}", "SCOTT")]
+        [TestCase("{:t:FIRST}", "SCOTT")]
+        [TestCase("{:t:last}", "rippey")]
+        [TestCase("{:t:LAST}", "rippey")]
+        [TestCase("{:t:nested}", "SCOTT rippey")] 
+        [TestCase("{:t:NESTED}", "SCOTT rippey")]
+        [TestCase("{:t:NeStEd}", "SCOTT rippey")]
+        [TestCase("{:t:fIrStLaSt}", "Scott Rippey")]
         public void Templates_can_be_case_insensitive_and_overwrite_each_other(string format, string expected)
         {
             var smart = GetFormatterWithRegisteredTemplates(CaseSensitivityType.CaseInsensitive);;
