7c3648856d105725a5c50a3415703e46b1f3be70
axunonb <axunonb@users.noreply.github.com>
2025-10-05 20:31:09 +0200

    Import CLDR plural rules
    Use a T4 complate to import CLDR plural rules and generate class `CldrPluralRules`

---
+++ src/SmartFormat.Tests/Utilities/CldrPluralRulesTests.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using SmartFormat.Tests.TestUtils;
+using SmartFormat.Utilities;
+
+namespace SmartFormat.Tests.Utilities;
+
+[TestFixture]
+internal class CldrPluralRulesTests
+{
+    private static readonly Dictionary<string, List<PluralRuleInfo>> Samples
+        = CldrPluralRuleSamples.LoadRules(GetCldrPluralizationAssetFilePath());
+
+    private static string GetCldrPluralizationAssetFilePath()
+    {
+        var sourceRoot = Path.GetFullPath(Path.Combine(TestContext.CurrentContext.TestDirectory));
+        while (!sourceRoot.EndsWith($"{Path.DirectorySeparatorChar}src"))
+        {
+            sourceRoot = Path.GetFullPath(Path.Combine(sourceRoot, ".."));
+            // If root is reached
+            if (sourceRoot.EndsWith($"{Path.DirectorySeparatorChar}"))
+                throw new FileNotFoundException("Could not find the 'src' folder.");
+        }
+
+        var filePath = Path.Combine(sourceRoot, "SmartFormat", "Utilities", "plurals.json");
+        return filePath;
+    }
+
+    // Test case source: just gets the locale keys
+    public static IEnumerable<TestCaseData> GetLocales =>
+        Samples.Keys.Select(l => new TestCaseData(l));
+
+    [TestCaseSource(nameof(GetLocales))]
+    public void Test_CldrPluralRules_Against_CldrExampleData(string locale)
+    {
+        var rule = CldrPluralRules.GetPluralRule(locale);
+
+        var sample = Samples[locale];
+        using (Assert.EnterMultipleScope())
+        {
+            foreach (var ruleInfo in sample)
+            {
+                foreach (var val in ruleInfo.IntegerSamples)
+                {
+                    _ = Enum.TryParse<PluralCategory>(ruleInfo.Category, true, out var cat);
+                    var message = $"Locale '{locale}' with integer '{val}' should be '{cat}'";
+                    var catFound = rule(val, 0);
+
+                    switch (ruleInfo.Category)
+                    {
+                        case "zero":
+                            Assert.That(catFound, Is.EqualTo(PluralCategory.Zero),
+                                message);
+                            break;
+                        case "one":
+                            Assert.That(catFound, Is.EqualTo(PluralCategory.One),
+                                message);
+                            break;
+                        case "two":
+                            Assert.That(catFound, Is.EqualTo(PluralCategory.Two),
+                                message);
+                            break;
+                        case "few":
+                            Assert.That(catFound, Is.EqualTo(PluralCategory.Few),
+                                message);
+                            break;
+                        case "many":
+                            Assert.That(catFound, Is.EqualTo(PluralCategory.Many),
+                                message);
+                            break;
+                        case "other":
+                            Assert.That(catFound, Is.EqualTo(PluralCategory.Other),
+                                message);
+                            break;
+                    }
+                }
+
+                foreach (var val in ruleInfo.DecimalSamples)
+                {
+                    _ = Enum.TryParse<PluralCategory>(ruleInfo.Category, true, out var cat);
+                    var samplePluralIndex = rule(val, 0);
+                    var message = $"Locale '{locale}' with decimal '{val.ToString(CultureInfo.InvariantCulture)}' should be '{cat}'";
+
+                    switch (ruleInfo.Category)
+                    {
+                        case "zero":
+                            Assert.That(samplePluralIndex, Is.EqualTo(PluralCategory.Zero),
+                                message);
+                            break;
+                        case "one":
+                            Assert.That(samplePluralIndex, Is.EqualTo(PluralCategory.One),
+                                message);
+                            break;
+                        case "two":
+                            Assert.That(samplePluralIndex, Is.EqualTo(PluralCategory.Two),
+                                message);
+                            break;
+                        case "few":
+                            Assert.That(samplePluralIndex, Is.EqualTo(PluralCategory.Few),
+                                message);
+                            break;
+                        case "many":
+                            Assert.That(samplePluralIndex, Is.EqualTo(PluralCategory.Many),
+                                message);
+                            break;
+                        case "other":
+                            Assert.That(samplePluralIndex, Is.EqualTo(PluralCategory.Other),
+                                message);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    [Test]
+    public void UnknownIsoCode_ShouldReturn_SingularRule()
+    {
+        Assert.That(() => CldrPluralRules.GetPluralRule(null).Method.Name, Is.EqualTo("Singular"));
+        Assert.That(() => CldrPluralRules.GetPluralRule("unknown").Method.Name, Is.EqualTo("Singular"));
+    }
+
+    [Test]
+    public void RestoreDefault_ShouldClear_CustomRules()
+    {
+        var defaultSize = CldrPluralRules.IsoLangToDelegate.Count;
+        CldrPluralRules.IsoLangToDelegate.Add("unknown", (n, i) => PluralCategory.One);
+        var sizeAfterAdding = CldrPluralRules.IsoLangToDelegate.Count;
+        CldrPluralRules.RestoreDefault();
+        var sizeAfterRestoring = CldrPluralRules.IsoLangToDelegate.Count;
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(sizeAfterAdding, Is.EqualTo(defaultSize + 1));
+            Assert.That(sizeAfterRestoring, Is.EqualTo(defaultSize));
+        }
+    }
+
+#if NET6_0_OR_GREATER
+    [Test, Explicit]
+    public void Create_CldrPluralRules_Using_CldrPluralRuleGenerator()
+    {
+        // Creates or overwrites the CldrPluralRules.cs
+        // file from the plurals.json asset file.
+        Assert.That(() =>
+        {
+            var assetPath = GetCldrPluralizationAssetFilePath();
+            var jsonFile = Path.GetFullPath(Path.Combine(assetPath, "plurals.json"));
+            var csFile = Path.GetFullPath(Path.Combine(assetPath, "CldrPluralRules.cs"));
+            CldrPluralRuleGenerator.Generate(jsonFile, csFile);
+        }, Throws.Nothing);
+    }
+#endif
+
+}
